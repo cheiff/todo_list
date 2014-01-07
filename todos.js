a9c16fc9e5320ad86d3128f77276735a84fc220d
Josiah Berkebile <josiah.berkebile@cerner.com>
2014-01-07 14:02:00 -0600

    Added ability to add items to todo list

--- todos.js
+++ todos.js
@@ -1,7 +1,20 @@
+var EnterKey = 13;
+
 $(document).ready(function() {
-  // $('#todoapp').fadeOut();
-  $('#new-todo').on('click', function() {
-    $element = $('#main');
-    $element.show();
+  $('#new-todo').keypress(function(e) {
+    if (e.which === EnterKey) {
+      $element = $('#main');
+      $element.show();
+      $todoList = $('#todo-list');
+      $todoList.append(
+        "<li>" +
+          "<div class='view'>" +
+            "<input class='toggle' type='checkbox'>" +
+            "<label>" + " " + $('#new-todo').val() + "</label>" +
+            "<a class='destroy'></a>" +
+          "</div>" +
+        "</li>"
+      );
+    }
   });
 });
